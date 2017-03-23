c966b9d8b8798f1d5fa4d4aee1a2ab15a85ca235
doni <doni@dev-machine>
2017-03-23 00:57:50 -0300

    Inserindo README.md e atualizando identação para tabs

---
+++ README.md
@@ -0,0 +1,11 @@
+# Estudando o Ionic
+
+## Para rodar o aplicativo
+
+1. Faça o download do código
+2. Instale o NodeJS, Cordova e o Ionic
+3. Pelo terminal entre na pasta do projeto e digite:
+	```npm install```	
+> Esse comando é utilizado para baixar e instalar todas as dependências necessárias do projeto
+4. Depois de instalado digite:
+	```ionic serve```
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,22 +7,22 @@ import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
 
 @NgModule({
-  declarations: [
-    MyApp,
-    HomePage
-  ],
-  imports: [
-    IonicModule.forRoot(MyApp)
-  ],
-  bootstrap: [IonicApp],
-  entryComponents: [
-    MyApp,
-    HomePage
-  ],
-  providers: [
-    StatusBar,
-    SplashScreen,
-    {provide: ErrorHandler, useClass: IonicErrorHandler}
-  ]
+	declarations: [
+		MyApp,
+		HomePage
+	],
+	imports: [
+		IonicModule.forRoot(MyApp)
+	],
+	bootstrap: [IonicApp],
+	entryComponents: [
+		MyApp,
+		HomePage
+	],
+	providers: [
+		StatusBar,
+		SplashScreen,
+		{provide: ErrorHandler, useClass: IonicErrorHandler}
+	]
 })
 export class AppModule {}
